9c125cb6f3443556bfc55f35d51ea9170205924b
LuLi <3048715073@qq.com>
2025-06-29 20:55:26 +0800

    2.16-2.65

--- 2.c
+++ 2.c
@@ -89,3 +89,83 @@
 
 
 
+//2.61
+// #include<stdio.h>
+// int A(int x){  //判断是否全为1
+//     return !~x;
+// }
+// int B(int x){ //判断是否全为0
+//     return !x;
+// }
+// int C(int x){ //判断最低字节位是否为 0xff
+//     return A(x|~0xff);
+// }
+// int D(int x){ //判断最高字节位是否为 0x00
+//     return B((x >> ((sizeof(int)-1) << 3)) & 0xff);
+// }
+
+
+
+//2.62(是否是算数右移)
+// #include<stdio.h>
+// int int_shifts_are_arithmetic(){
+//     int num=-1;
+//     return !(num^(num>>1));  //采用异或判断相等
+// }
+// int main(){
+//     if(int_shifts_are_arithmetic()) printf("yes\n");
+//     else printf("no\n");
+//     return 0;
+// }
+
+
+
+//2.63
+// #include <stdio.h>
+// unsigned srl(unsigned x, int k) {  //算数右移完成逻辑右移
+//    unsigned xsra = (int) x >> k;
+//    int w=sizeof(int)<<3;
+//    int mask= (int)-1 <<(w-k);
+//    return xsra&(~mask);
+// }
+// int sra(int x, int k) {  //逻辑右移完成算数右移
+//     int xsrl=(unsigned)x>>k;
+//     int w=sizeof(int)<<3;
+//     int mask = (int) -1 << (w - k);
+//     int m = 1 << (w - 1);
+//     mask &= ! (x & m) - 1;
+//     return xsrl | mask;
+// }
+// int main() {
+//   return 0;
+// }
+
+
+
+//2.65 (判断是否有奇数个1)
+//有两个相同的一 就给他消掉（这个思想挺秒的）
+//前一半和后一半 如果都为1 那么消掉，如果不同，保留一（这样不会减少1的个数
+
+// int odd_ones(unsigned x){  
+//    x ^= x >> 16;
+//    x ^= x >> 8;
+//    x ^= x >> 4;
+//    x ^= x >> 2;
+//    x ^= x >> 1;
+//    x &= 0x1;
+//   return x;
+// }
+
+
+
+//2.66
+
+/*
+ * Generate mask indicating leftmost 1 in x. Assume w=32
+ * For example, 0xFF00 -> 0x8000, and 0x6000 -> 0x4000.
+ * If x = 0, then return 0
+*/
+
+int leftmost_one(unsigned x){
+   
+}
